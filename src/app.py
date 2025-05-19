a4560e45cd531d5c7fdd04140f43e4ef95d89b45
masayasviel <33654883+masayasviel@users.noreply.github.com>
2025-05-19 11:10:41 +0900

    Update app.py

--- src/app.py
+++ src/app.py
@@ -1,8 +1,7 @@
-from fastapi import Depends, FastAPI, HTTPException
+from fastapi import FastAPI, HTTPException
 from sqlalchemy import select
-from sqlalchemy.orm import Session
 from pydantic import BaseModel
-from db import SessionLocal
+from db import SessionDep
 from models import User, Post
 
 app = FastAPI()
@@ -12,22 +11,15 @@ class PostCreate(BaseModel):
     user_id: int
     contents: str
 
-# レスポンス用のスキーマ
+
 class PostResponse(BaseModel):
     user_id: int
     contents: str
     user_name: str
 
 
-def get_db():
-    db = SessionLocal()
-    try:
-        yield db
-    finally:
-        db.close()
-
 @app.post("/top", response_model=PostResponse)
-def create_post(post_data: PostCreate, db: Session = Depends(get_db)):
+def create_post(post_data: PostCreate, db: SessionDep):
     user = db.query(User).filter(User.id == post_data.user_id).first()
     if not user:
         raise HTTPException(status_code=404, detail="User not found")
